01313d87f894347f99f2bd406bd644f94492bd90
Pawan Kumar Pathak <96225167+PawanIITM2021@users.noreply.github.com>
2021-12-21 15:26:08 +0530

    Add files via upload

---
+++ Digi_clo.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    Label.config(text=string)
+    Label.after(1000,time)
+
+Label = Label(root,font=("digital-7", 80),background = "black", foreground = "cyan")
+Label.pack(anchor = 'center')
+time()
+
+mainloop()
+
+
